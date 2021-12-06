602cbf1a5e81039903034da2cd896abd8fcdb2d9
Hiba Saqib <hibasaqib72@gmail.com>
2021-12-06 19:38:15 +0500

    App bar is properly working as navigating or action bar

--- app/src/main/java/com/example/appbars/MainActivity.java
+++ app/src/main/java/com/example/appbars/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.appbars;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
 public class MainActivity extends AppCompatActivity {
@@ -19,5 +24,22 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu){
        getMenuInflater().inflate(R.menu.menu,menu);
        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+            case R.id.action_favorite1:
+                Toast.makeText(getApplicationContext(),"Fav", Toast.LENGTH_LONG).show();
+            case R.id.action_TASKS:
+                TextView textView = findViewById(R.id.textView2);
+                textView.setText("Action task was clicked");
+            default:
+// If we got here, the user's action was not recognized.
+// Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
